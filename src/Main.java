00b54661e34afc846251d3f3245ce1a47c9dc2e4
abalmasDA <abalmasit@gmail.com>
2023-07-13 21:17:53 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+
+/**
+ * Створіть файл, запишіть у нього довільні дані та закрийте файл.
+ * Потім знову відкрийте цей файл, прочитайте дані і виведіть їх на консоль.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("someFile.bin"))) {
+            outputStream.writeInt(123456789);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try (DataInputStream inputStream = new DataInputStream(new FileInputStream("someFile.bin"))) {
+            int someInt = inputStream.readInt();
+            System.out.println(someInt);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 }
